9d5a2ec5857da0c8e7859ba1496be2d23a0bd664
TOSHIUBA\Brenda <brendamorpe@gmail.com>
2018-08-26 16:54:35 -0500

    Cambios de interfaz

--- docs/src/js/data.js
+++ docs/src/js/data.js
@@ -3,7 +3,7 @@ const getMovies = url => fetch(url).then(response => response.json());
 document.getElementById('movies').addEventListener('change', () => {
   const movies = document.getElementById('movies').value;
   let Printposter = '';
-
+  
   getMovies(`http://www.omdbapi.com/?s=${movies}&apikey=51976a0e`)
     .then((resultados) => {
       console.log(resultados.Search);
@@ -11,34 +11,56 @@ document.getElementById('movies').addEventListener('change', () => {
       for (let i = 0; i < resultados.Search.length; i++) {
         const allMovies = resultados.Search[i];
 
-        console.log(allMovies);
+        // console.log(allMovies);
+
+       
+        Poster = '';
+        if (allMovies.Poster === 'N/A') {
+          Poster = 'images/nodisponible.png';
+        } else {
+          Poster = allMovies.Poster;
+        }
+
 
         Printposter
      += `
           <div class="card" style="width: 18rem;">
-          <img class="card-img-top" src="${allMovies.Poster}" alt="Card image cap">
+          <img class="height-img" src="${Poster}" alt="Card image cap">
           <div class="card-body">
-            <h5 class="card-title">${allMovies.Title}</h5>
-           
+            
+            <p><b>${allMovies.Title}</b></p>
             <!-- Button trigger modal -->
 <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#${allMovies.imdbID}">
  +Info
 </button>
           </div>
         </div>
+
+        
+
+
+
+
+
+
+
 <!-- Modal -->
 <div class="modal fade" id="${allMovies.imdbID}" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
   <div class="modal-dialog" role="document">
     <div class="modal-content">
       <div class="modal-header">
-        <h7 class="modal-title" id="exampleModalLabel">${allMovies.Title}</h7>
+        <h7 class="modal-title" id="exampleModalLabel"> <p><b>${allMovies.Title}</b></p></h7>
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
           <span aria-hidden="true">&times;</span>
         </button>
       </div>
       <div class="modal-body">
+      <center>
+      <p><img class="height-img" src="${Poster}"></p>
+      
       <p>Año: ${allMovies.Year} </p>
       <p>Tipo: ${allMovies.Type}</p>
+      </center>
       </div>
       <div class="modal-footer">
         <button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
@@ -63,35 +85,46 @@ document.getElementById('button-Search').addEventListener('click', () => {
       for (let i = 0; i < resultados.Search.length; i++) {
         const allMovies = resultados.Search[i];
 
-        console.log(allMovies);
+        // console.log(allMovies);
+
+        Poster = '';
+        if (allMovies.Poster === 'N/A') {
+          Poster = 'images/nodisponible.png';
+        } else {
+          Poster = allMovies.Poster;
+        }
 
 
         Printposter += `
-    
-<div class="card" style="width: 18rem;">
-<img class="card-img-top" src="${allMovies.Poster}" alt="Card image cap">
-<div class="card-body">
-<h5 class="card-title">${allMovies.Title}</h5>
 
-<!-- Button trigger modal -->
+
+
+<div class="card" style="width: 18rem;">
+          <img class="height-img" src="${Poster}" alt="Card image cap">
+          <div class="card-body">
+            
+            <p><b>${allMovies.Title}</b></p>
+            <!-- Button trigger modal -->
 <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#${allMovies.imdbID}">
-+Info
+ +Info
 </button>
-</div>
-</div>   
+          </div>
+        </div>
 
 <!-- Modal -->
 <div class="modal fade" id="${allMovies.imdbID}" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
 <div class="modal-dialog" role="document">
 <div class="modal-content">
 <div class="modal-header">
-<h5 class="modal-title" id="exampleModalLabel">${allMovies.Title}</h5>
+<h5 class="modal-title" id="exampleModalLabel"> <p><b>${allMovies.Title}</b></p></h5>
 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
  <span aria-hidden="true">&times;</span>
 </button>
 </div>
 <div class="modal-body">
 <p class="card-text"><img src="${allMovies.Poster}" class="img-fluid img-thumbnail" alt="Responsive image">foto </p>
+
+
 <p>Año: ${allMovies.Year} </p>
 <p>Tipo: ${allMovies.Type}</p>
 </div>
--- docs/src/js/login.js
+++ docs/src/js/login.js
@@ -1,4 +1,6 @@
 let signLogin = document.getElementById('button-login-entrar');
+
+
 // Ingresa el usuario
 signLogin.addEventListener('click', event => {
   let emailUser = document.getElementById('email').value;
@@ -17,5 +19,26 @@ signLogin.addEventListener('click', event => {
       let errorMessage = 'Escribe un usuario o contraseña validos';
       console.log(errorCode);
       alert(errorMessage);
+    });
+});
+
+
+let registryUser = document.getElementById('Registrar');
+
+registryUser.addEventListener('click', event => {
+  let regName = document.getElementById('registryName').value;
+  let regEmail = document.getElementById('registryEmail').value;
+  let regPassword = document.getElementById('registryPassword').value;
+
+
+  firebase.auth().createUserWithEmailAndPassword(regEmail, regPassword)
+    .catch(function(error) {
+    // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+
+      console.log(errorCode);
+      console.log(errorMessage);
+    // ...
     });
 });
